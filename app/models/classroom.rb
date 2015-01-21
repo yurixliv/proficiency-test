262a9370a075e4160459b6332605ac72ac428787
Yuri Oliveira <yuri.oliveiraxli@gmail.com>
2015-01-21 09:07:02 -0300

    details

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -3,10 +3,11 @@ class Classroom < ActiveRecord::Base
   belongs_to :student
   belongs_to :course
 
+  validates_presence_of  :student_id, :course_id
   before_create :record_signup
 
   private
     def record_signup
-      self.entry_at = Date.today
+      self.entry_at = Time.now
     end
 end
